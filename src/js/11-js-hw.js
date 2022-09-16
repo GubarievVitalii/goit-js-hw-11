c9772a0a07f69c593bc39675d999f3f284067237
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-09-16 17:57:15 +0300

    3

--- src/js/11-js-hw.js
+++ src/js/11-js-hw.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 
 const formEl = document.querySelector('#search-form');
@@ -8,20 +8,37 @@ const formEl = document.querySelector('#search-form');
 const btnEl = document.querySelector('button');
 const galleryEl = document.querySelector('.gallery');
 
+const galerySimple = new SimpleLightbox('.photo-card', {
+  captionsData: 'alt',
+  overlayOpacity: 0.8,
+  showCounter: false,
+  widthRatio: 0.6,
+  captionDelay: 250,
+});
+
 formEl.addEventListener('submit', onSearch);
 
 function onSearch(event) {
   event.preventDefault();
   const inputEl = event.target.elements.searchQuery.value;
-
+  if (!inputEl) {
+    return Notify.failure(
+      'Sorry, your query is empty, please, make your choice'
+    );
+  }
   axios
     .get(
       `https://pixabay.com/api/?key=29860277-84021bf85cd78542af410165f&q=${inputEl}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
     )
     .then(response => response.data)
-    .then(data => {
-      console.log(data);
-      const cardMarkup = data.hits
+    .then(({ hits }) => {
+      if (hits.length === 0) {
+        formEl.reset();
+        return Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      const cardMarkup = hits
         .map(
           ({
             webformatURL,
@@ -32,8 +49,9 @@ function onSearch(event) {
             comments,
             downloads,
           }) => {
+            formEl.reset();
             return `<div class="photo-card">
-            <a href="${largeImageURL}">
+            <a class="photo-card__link" href="${largeImageURL}">
             <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
             <div class="info">
               <p class="info-item">
@@ -55,11 +73,7 @@ function onSearch(event) {
         )
         .join('');
       galleryEl.insertAdjacentHTML('afterbegin', cardMarkup);
+      galerySimple.refresh();
     })
     .catch(error => console.log(error));
 }
-
-const galerySimple = new SimpleLightbox('.photo-card', {
-  captionDelay: 250,
-});
-// galerySimple.refresh();
--- src/js/axios.js
+++ src/js/axios.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios
-  .get(
-    `https://pixabay.com/api/?key=29860277-84021bf85cd78542af410165f&q=${inputEl}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
-  )
-  .then(function (response) {
-    // обробка успішного запиту
-    console.log(response);
-  })
-  .catch(function (error) {
-    // обробка помилки
-    console.log(error);
-  })
-  .then(function () {
-    // виконується завжди
-  });
+// axios
+//   .get(
+//     `https://pixabay.com/api/?key=29860277-84021bf85cd78542af410165f&q=${inputEl}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+//   )
+//   .then(function (response) {
+//     // обробка успішного запиту
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // обробка помилки
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // виконується завжди
+//   });
